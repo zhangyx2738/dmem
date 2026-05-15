59e33d4d8731f6fb15ffee12639eb988f359b366
zhangyx2738 <Yuxin.Zhang@mckesson.ca>
2026-05-15 10:38:42 -0400

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class GenieSpaceConfig:
 
 GENIE_SPACES = [
     GenieSpaceConfig(
-        space_id="01f14fd33f431c5fbb9b3bb6a8a39d57",
+        space_id="01f13a788d11117d9f19087db499b1bc",
         name="DMEM Audit",
         description=(
             """This space provides data that should be filtered by buying groups (a combination of chain_id, nationalgroup, and account classification code)."""
